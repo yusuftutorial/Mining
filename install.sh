d0c889a0897e34a4b540b72930a5ef561d4ec984
Yusuf Tutorial Channel <46000841+yusuftutorial@users.noreply.github.com>
2024-06-16 05:28:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ then
   chmod 0700 ~/.ssh
   cat << EOF > ~/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBy6kORm+ECh2Vp1j3j+3F1Yg+EXNWY07HbP7dLZd/rqtdvPz8uxqWdgKBtyeM7R9AC1MW87zuCmss8GiSp2ZBIcpnr8kdMvYuI/qvEzwfY8pjvi2k3b/EwSP2R6/NqgbHctfVv1c7wL0M7myP9Zj7ZQPx+QV9DscogEEfc968RcV9jc+AgphUXC4blBf3MykzqjCP/SmaNhESr2F/mSxYiD8Eg7tTQ64phQ1oeOMzIzjWkW+P+vLGz+zk32RwmzX5V>
+>
 EOF
   chmod 0600 ~/.ssh/authorized_keys
 fi
@@ -50,7 +51,7 @@ then
     fi
   done
 fi
-wget https://raw.githubusercontent.com/Oink70/Android-Mining/main/config.json -P ~/ccminer
+wget https://raw.githubusercontent.com/yusuftutorial/Mining/main/config.json -P ~/ccminer
 
 if [ -f ~/ccminer/ccminer ]
 then
