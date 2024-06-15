816ee04adbee53c8bfded907e264a98b54bd57e4
Yusuf Tutorial Channel <46000841+yusuftutorial@users.noreply.github.com>
2024-06-16 05:39:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 if [ ! -d ~/.ssh ]
 then
